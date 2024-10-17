612a293475d9c9d9e78bd71725f84452ee1ee5a2
venkatdanush988 <22hr1a3205@mtieat.org>
2024-10-16 23:35:12 -0700

    Create set2.py

---
+++ set2.py
@@ -0,0 +1,8 @@
+***
+2) Write a program to get input in a single line separated by space and print the values of set in single line separated by space.
+Sample Input:
+3 1 5 4 2
+Sample Output:
+1 2 3 4 5
+
+***
